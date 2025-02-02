537c989ab931a078a2f07f0b80a98949b0a00812
jke94 <jcarracedo.dev@gmail.com>
2025-02-02 14:10:39 +0100

    Update to use new discovered web api endpoint.

--- castro_weather.py
+++ castro_weather.py
@@ -1,47 +1,53 @@
 import argparse
 import json
-import requests
 import os
+import requests
+
+from datetime import datetime, timedelta
 
 def wind_direction_calculataion(wind_direction_avg:int) -> str:
 
-    if 0 <= wind_direction_avg <= 22.5 or 337.5 <= wind_direction_avg <= 360:
-        return "N"
-    elif 22.5 <= wind_direction_avg < 67.5:
-        return "NE"
-    elif 67.5 <= wind_direction_avg < 112.5:
-        return "E" 
-    elif 112.5 <= wind_direction_avg < 157.5:
-        return "SE"
-    elif 157.5 <= wind_direction_avg < 202.5:
-        return "S"
-    elif 202.5 <= wind_direction_avg < 247.5:
-        return "SW"
-    elif 247.5 <= wind_direction_avg < 292.5:
-        return "W"
-    elif 292.5 <= wind_direction_avg < 337.5:
-        return "NW"    
+    wind_directions = [
+        "N", 
+        "NNE", 
+        "NE", 
+        "ENE",
+        "E", 
+        "ESE", 
+        "SE", 
+        "SSE",
+        "S", 
+        "SSW", 
+        "SW", 
+        "WSW", 
+        "W", 
+        "WNW", 
+        "NW", 
+        "NNW"
+    ]
     
-    return "NONE"
+    wind_direction = wind_directions[int((wind_direction_avg % 360) / 22.5)]
+
+    return wind_direction
 
 def risk_UV_calculation(uv:int) -> str:
 
     if 0 <= uv <= 2:
-        return "Bajo"
+        return "Low"
     elif 3 <= uv <= 5:
-        return "Moderado"
+        return "Medium"
     elif 6 <= uv <= 7:
-        return "Alto"
+        return "High"
     elif 8 <= uv <= 10:
-        return "Muy alto"
+        return "Very high"
     elif 11 <= uv:
-        return "Extremo"
+        return "Extremely high"
 
     return "NONE"
 
-def obtener_datos_meteorologicos(station_id:str, api_key:str) -> dict:
+def obtener_datos_meteorologicos(station_id:str, api_key:str, date:str) -> dict:
     
-    url = f"https://api.weather.com/v2/pws/dailysummary/3day?apiKey={api_key}&stationId={station_id}&numericPrecision=decimal&format=json&units=m"
+    url = f"https://api.weather.com/v2/pws/history/daily?stationId={station_id}&format=json&units=m&date={date}&apiKey={api_key}&numericPrecision=decimal"
 
     response = requests.get(url)
 
@@ -51,47 +57,35 @@ def obtener_datos_meteorologicos(station_id:str, api_key:str) -> dict:
     try:
         data = response.json()
 
-        # Get data.
-        obsTimeUtc = data['summaries'][1]['obsTimeUtc']
-        tempHigh = data['summaries'][1]['metric']['tempHigh']
-        tempAvg = data['summaries'][1]['metric']['tempAvg']
-        tempLow = data['summaries'][1]['metric']['tempLow']
-        humidityHigh = data['summaries'][1]['humidityHigh']
-        humidityLow = data['summaries'][1]['humidityLow']
-        humidityAvg = data['summaries'][1]['humidityAvg']        
-        precipTotal = data['summaries'][1]['metric']['precipTotal']
-        pressureMax = data['summaries'][1]['metric']['pressureMax']
-        pressureMin = data['summaries'][1]['metric']['pressureMin']
-        windgustHigh = data['summaries'][1]['metric']['windgustHigh']
-        windGustAvg = data['summaries'][1]['metric']['windgustAvg']
-        windSpeedHigh = data['summaries'][1]['metric']['windspeedHigh']
-        windspeedAvg = data['summaries'][1]['metric']['windspeedAvg']
-        winddirAvg = data['summaries'][1]['winddirAvg']
-        uvIndexHigh = data['summaries'][1]['uvHigh']
-        solarRadiationHigh = data['summaries'][1]['solarRadiationHigh']
+        metric_data = data["observations"][0]['metric']
         
-        wind_direction_avg = wind_direction_calculataion(winddirAvg)
-        uvHighRisk = risk_UV_calculation(uvIndexHigh)
+        wind_direction_avg = wind_direction_calculataion(data["observations"][0]["winddirAvg"])
+        uvHighRisk = risk_UV_calculation(data["observations"][0]["uvHigh"])
+
+        formatted_date = datetime.strptime(date, "%Y%m%d").strftime("%Y-%m-%d")
 
         output = {
-            "Date": obsTimeUtc,
-            "TemperatureHigh": tempHigh,
-            "TemperatureAvg": tempAvg,
-            "TemperatureLow": tempLow,
-            "HumidityHigh" : humidityHigh,
-            "HumidityLow" : humidityLow,
-            "HumidityAvg" : humidityAvg,
-            "PricipitationTotal": precipTotal,
-            "PressureMax": pressureMax,
-            "PressureMin": pressureMin,
-            "WindSpeedHigh": windSpeedHigh,      
-            "WindSpeedAvg": windspeedAvg,      
-            "WindGustHigh": windgustHigh,
-            "WindGustAvg": windGustAvg,
+            "Date": formatted_date,
+            "TemperatureHigh": metric_data["tempHigh"],
+            "TemperatureAvg": metric_data["tempAvg"],
+            "TemperatureLow": metric_data["tempLow"],
+            "DewPointHigh": metric_data["dewptHigh"],
+            "DewPointLow": metric_data["dewptLow"],
+            "DewPointAvg": metric_data["dewptAvg"],
+            "HumidityHigh": data["observations"][0]["humidityHigh"],
+            "HumidityLow": data["observations"][0]["humidityLow"],
+            "HumidityAvg": data["observations"][0]["humidityAvg"],
+            "PricipitationTotal": metric_data["precipTotal"],
+            "PressureMax": metric_data["pressureMax"],
+            "PressureMin": metric_data["pressureMin"],
+            "WindSpeedHigh": metric_data["windspeedHigh"],
+            "WindSpeedAvg": metric_data["windspeedAvg"],
+            "WindGustHigh": metric_data["windgustHigh"],
+            "WindGustAvg": metric_data["windgustAvg"],
             "WindDirectionAvg": wind_direction_avg,
             "UvHighRisk": uvHighRisk,
-            "UvIndexHigh": uvIndexHigh,
-            "SolarRadiationHigh" : solarRadiationHigh
+            "UvIndexHigh": data["observations"][0]["uvHigh"],
+            "SolarRadiationHigh" : data["observations"][0]["solarRadiationHigh"]
         }
 
         return output
@@ -112,9 +106,12 @@ if __name__ == "__main__":
 
     station_id = args.station_id or os.getenv("STATION_ID")
     api_key = args.api_key or os.getenv("API_KEY")
+    
+    yesterday = datetime.now() - timedelta(days=1)
+    yesterday_formatted_date = yesterday.strftime("%Y%m%d")   
 
     if not station_id or not api_key:
         print("Error: The environment variables STATION_ID and API_KEY must be defined or passed as arguments.")
     else:
-        resultado = obtener_datos_meteorologicos(station_id, api_key)
+        resultado = obtener_datos_meteorologicos(station_id, api_key, yesterday_formatted_date)
         print(json.dumps(resultado, indent=4))
