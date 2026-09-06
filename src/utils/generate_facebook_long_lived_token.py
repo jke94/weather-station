02ce91487fad66ef43d1c15f7526dbf023ae9aa0
Javier Carracedo <53972851+jke94@users.noreply.github.com>
2026-09-06 22:47:36 +0200

    Fix morning daily scheduler tweet in facebook file (#24)
    * Remove redundant morning daily scheduler step from Facebook workflow
    
    * Ensure proper exit status in main function of Facebook post script
    
    * Refactor Facebook token generation and posting scripts; update README for clarity

--- src/utils/generate_facebook_long_lived_token.py
+++ src/utils/generate_facebook_long_lived_token.py
@@ -1,19 +1,30 @@
 import requests
-import argparse
 import sys
+import os
 
+from dotenv import load_dotenv
 
-GRAPH_API_VERSION = "v19.0"
+# Reads variables from a .env file and sets them in os.environ
+load_dotenv()
 
+GRAPH_API_VERSION = "v25.0"
 
-def get_long_lived_token(app_id, app_secret, short_token):
+FACEBOOK_ACCESS_TOKEN = os.getenv('FACEBOOK_ACCESS_TOKEN')
+FACEBOOK_APP_SECRET = os.getenv('FACEBOOK_APP_SECRET')
+FACEBOOK_PAGE_ID = os.getenv('FACEBOOK_PAGE_ID')
+
+def get_long_lived_token(
+        app_page_id,
+        app_secret,
+        access_token
+    ):
     url = f"https://graph.facebook.com/{GRAPH_API_VERSION}/oauth/access_token"
 
     params = {
         "grant_type": "fb_exchange_token",
-        "client_id": app_id,
+        "client_id": app_page_id,
         "client_secret": app_secret,
-        "fb_exchange_token": short_token
+        "fb_exchange_token": access_token
     }
 
     r = requests.get(url, params=params)
@@ -27,18 +38,11 @@ def get_long_lived_token(app_id, app_secret, short_token):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Generate Facebook Long-Lived User Token")
-
-    parser.add_argument("--app_id", required=True)
-    parser.add_argument("--app_secret", required=True)
-    parser.add_argument("--short_token", required=True)
-
-    args = parser.parse_args()
 
     result = get_long_lived_token(
-        args.app_id,
-        args.app_secret,
-        args.short_token
+        FACEBOOK_PAGE_ID,
+        FACEBOOK_APP_SECRET,
+        FACEBOOK_ACCESS_TOKEN
     )
 
     print("\nLong-Lived Token generado:\n")
@@ -47,7 +51,6 @@ def main():
     if "expires_in" in result:
         days = int(result["expires_in"]) // 86400
         print(f"\nExpira en aproximadamente {days} días")
-
 
 if __name__ == "__main__":
     main()
